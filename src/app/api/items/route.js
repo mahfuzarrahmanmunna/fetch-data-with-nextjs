1235443620a9d4913b6044d97f284077e746e09c
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-07-28 03:14:49 +0600

    added env.local and dbconnect file ;

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,13 +1,16 @@
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
 export async function GET() {
-    const res = await fetch('https://data.mongodb-api.com/...', {
-        headers: {
-            'Content-Type': 'application/json',
-            'API-Key': process.env.DATA_API_KEY,
-        },
-    })
-    const data = await res.json()
-
+    const data = {
+        message: "Successfully ge data",
+        error: false,
+        status: 200
+    }
     return Response.json({ data })
+}
+
+export async function POST(req) {
+    const postedData = await req.json()
+
+    return Response.json({ postedData })
 }
